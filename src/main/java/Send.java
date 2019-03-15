fba824d1240dec89e7b3e1d13a6adc9af8b42de4
meridith <mjunk@pillartechnology.com>
2019-03-15 13:48:35 -0500

    Added Rabbit MQ recieving message queue code

--- src/main/java/Send.java
+++ src/main/java/Send.java
@@ -2,15 +2,47 @@ import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
 
+import com.rabbitmq.client.DeliverCallback;
+
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 public class Send {
     private final static String QUEUE_NAME = "hello";
+
     public static void main(String[] argv) throws Exception {
+        receive();
+        send();
+    }
+
+    private static void receive() throws IOException, TimeoutException {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+
+       DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + message + "'");
+        };
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+
+
+    }
+
+    private static void send() throws IOException, TimeoutException {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            String message = "Hello World!";
+            String message = "Lets test this one more time..........!";
             channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
             System.out.println(" [x] Sent '" + message + "'");
         }
